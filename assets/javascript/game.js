063c2ddb68c4796aabb4eb0765c91b6c1e9e1ba1
Alan <a-ashbeck@northwestern.edu>
2016-10-24 15:21:50 -0500

    Add logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
         name: 'Luke Skywalker',
         health: 100,
         attack: 5,
+        attackIncrement: 5,
         image: 'assets/images/luke.jpg'
     };
 
@@ -10,6 +11,7 @@ $(document).ready(function() {
         name: 'Darth Vader',
         health: 180,
         attack: 25,
+        attackIncrement: 25,
         image: 'assets/images/vader.jpg'
     };
 
@@ -17,6 +19,7 @@ $(document).ready(function() {
         name: 'Obi-Wan Kenobi',
         health: 120,
         attack: 8,
+        attackIncremenet: 8,
         image: 'assets/images/kenobi.jpg'
     };
 
@@ -24,74 +27,88 @@ $(document).ready(function() {
         name: 'Kylo Ren',
         health: 150,
         attack: 20,
+        attackIncrement: 20,
         image: 'assets/images/kylo.jpg'
     };
 
     var mainCharacter = {};
-    var enemies = [];
     var defender = {};
 
-    // Creates html and outputs character object data for Luke Skywalker
-    $('.luke-skywalker').append(
-        '<div class="panel panel-default"><div class="panel-heading">' +
-        lukeSkywalker.name + '</div>' +
-        '<div class="panel-body"><img class="img-responsive" src="' +
-        lukeSkywalker.image + '"></div>' +
-        '<div class="panel-footer">' + lukeSkywalker.health + '</div></div>'
-    );
+    function createCharacters() {
+        // Creates html and outputs character object data for Luke Skywalker
+        $('.luke-skywalker').append(
+            '<div class="panel panel-default"><div class="panel-heading">' +
+            lukeSkywalker.name + '</div>' +
+            '<div class="panel-body"><img class="img-responsive" src="' +
+            lukeSkywalker.image + '"></div>' +
+            '<div class="panel-footer">' + lukeSkywalker.health + '</div></div>'
+        );
 
-    // Creates html and outputs character object data for Darth Vader
-    $('.darth-vader').append(
-        '<div class="panel panel-default"><div class="panel-heading">' +
-        darthVader.name + '</div>' +
-        '<div class="panel-body"><img class="img-responsive" src="' +
-        darthVader.image + '"></div>' +
-        '<div class="panel-footer">' + darthVader.health + '</div></div>'
-    );
+        // Creates html and outputs character object data for Darth Vader
+        $('.darth-vader').append(
+            '<div class="panel panel-default"><div class="panel-heading">' +
+            darthVader.name + '</div>' +
+            '<div class="panel-body"><img class="img-responsive" src="' +
+            darthVader.image + '"></div>' +
+            '<div class="panel-footer">' + darthVader.health + '</div></div>'
+        );
 
-    // Creates html and outputs character object data for Obi-Wan Kenobi
-    $('.obi-wan-kenobi').append(
-        '<div class="panel panel-default"><div class="panel-heading">' +
-        obiWanKenobi.name + '</div>' +
-        '<div class="panel-body"><img class="img-responsive" src="' +
-        obiWanKenobi.image + '"></div>' +
-        '<div class="panel-footer">' + obiWanKenobi.health + '</div></div>'
-    );
+        // Creates html and outputs character object data for Obi-Wan Kenobi
+        $('.obi-wan-kenobi').append(
+            '<div class="panel panel-default"><div class="panel-heading">' +
+            obiWanKenobi.name + '</div>' +
+            '<div class="panel-body"><img class="img-responsive" src="' +
+            obiWanKenobi.image + '"></div>' +
+            '<div class="panel-footer">' + obiWanKenobi.health + '</div></div>'
+        );
 
-    // Creates html and outputs character object data for Kylo Ren
-    $('.kylo-ren').append(
-        '<div class="panel panel-default"><div class="panel-heading">' +
-        kyloRen.name + '</div>' +
-        '<div class="panel-body"><img class="img-responsive" src="' +
-        kyloRen.image + '"></div>' +
-        '<div class="panel-footer">' + kyloRen.health + '</div></div>'
-    );
+        // Creates html and outputs character object data for Kylo Ren
+        $('.kylo-ren').append(
+            '<div class="panel panel-default"><div class="panel-heading">' +
+            kyloRen.name + '</div>' +
+            '<div class="panel-body"><img class="img-responsive" src="' +
+            kyloRen.image + '"></div>' +
+            '<div class="panel-footer">' + kyloRen.health + '</div></div>'
+        );
+    }
+
+    createCharacters();
 
     // Logic dictating where characters are assigned based on click order
     $('.character').on('click', function() {
         if (Object.keys(mainCharacter).length === 0) {
             if ($(this).hasClass('luke-skywalker')) {
-              mainCharacter = lukeSkywalker;
-              $('.darth-vader').appendTo('#enemies');
-              $('.obi-wan-kenobi').appendTo('#enemies');
-              $('.kylo-ren').appendTo('#enemies');
+                mainCharacter = lukeSkywalker;
+                $('.darth-vader').appendTo('#enemies');
+                $('.obi-wan-kenobi').appendTo('#enemies');
+                $('.kylo-ren').appendTo('#enemies');
             } else if ($(this).hasClass('darth-vader')) {
-              mainCharacter = darthVader;
-              $('.luke-skywalker').appendTo('#enemies');
-              $('.obi-wan-kenobi').appendTo('#enemies');
-              $('.kylo-ren').appendTo('#enemies');
+                mainCharacter = darthVader;
+                $('.luke-skywalker').appendTo('#enemies');
+                $('.obi-wan-kenobi').appendTo('#enemies');
+                $('.kylo-ren').appendTo('#enemies');
             } else if ($(this).hasClass('obi-wan-kenobi')) {
-              mainCharacter = obiWanKenobi;
-              $('.luke-skywalker').appendTo('#enemies');
-              $('.darth-vader').appendTo('#enemies');
-              $('.kylo-ren').appendTo('#enemies');
+                mainCharacter = obiWanKenobi;
+                $('.luke-skywalker').appendTo('#enemies');
+                $('.darth-vader').appendTo('#enemies');
+                $('.kylo-ren').appendTo('#enemies');
             } else {
-              mainCharacter = kyloRen;
-              $('.luke-skywalker').appendTo('#enemies');
-              $('.darth-vader').appendTo('#enemies');
-              $('.obi-wan-kenobi').appendTo('#enemies');
+                mainCharacter = kyloRen;
+                $('.luke-skywalker').appendTo('#enemies');
+                $('.darth-vader').appendTo('#enemies');
+                $('.obi-wan-kenobi').appendTo('#enemies');
             };
-            $(this).appendTo('#main-character');
+
+            $('.main-character').append(
+                '<div class="panel panel-info"><div class="panel-heading">' +
+                mainCharacter.name + '</div>' +
+                '<div class="panel-body"><img class="img-responsive" src="' +
+                mainCharacter.image + '"></div>' +
+                '<div class="panel-footer">' + mainCharacter.health +
+                '</div></div>'
+            );
+
+            $('#starting-characters').empty();
             $(this).addClass('chosen-character');
         } else if (
             (Object.keys(mainCharacter).length !== 0 &&
@@ -120,13 +137,82 @@ $(document).ready(function() {
             ) {
                 defender = kyloRen;
             };
-            $(this).appendTo('#defender');
+
+            $(this).hide();
+
+            $('.defender').append(
+                '<div class="panel panel-danger"><div class="panel-heading">' +
+                defender.name + '</div>' +
+                '<div class="panel-body"><img class="img-responsive" src="' +
+                defender.image + '"></div>' +
+                '<div class="panel-footer">' + defender.health +
+                '</div></div>'
+            );
         };
-        console.log(mainCharacter);
-        console.log(defender);
     });
 
-    
+    $('#attack').on('click', function() {
+        if (Object.keys(defender).length === 0) {
+            $('#status').empty();
+            $('#status').append(
+                '<h3>No enemy here</h3>'
+            );
+        } else {
+            $('#status').empty();
+            $('.main-character').empty();
+            $('.defender').empty();
+            defender.health = defender.health - mainCharacter.attack;
+            mainCharacter.health = mainCharacter.health - defender.attack;
+            $('.main-character').append(
+                '<div class="panel panel-info"><div class="panel-heading">' +
+                mainCharacter.name + '</div>' +
+                '<div class="panel-body"><img class="img-responsive" src="' +
+                mainCharacter.image + '"></div>' +
+                '<div class="panel-footer">' + mainCharacter.health +
+                '</div></div>'
+            );
+            $('.defender').append(
+                '<div class="panel panel-danger"><div class="panel-heading">' +
+                defender.name + '</div>' +
+                '<div class="panel-body"><img class="img-responsive" src="' +
+                defender.image + '"></div>' +
+                '<div class="panel-footer">' + defender.health +
+                '</div></div>'
+            );
+            $('#status').append(
+                '<p>You attacked ' + defender.name + ' for ' +
+                mainCharacter.attack +
+                ' damage.</p><p>' + defender.name + ' attacked you back for ' +
+                defender.attack + ' damage.</p>'
+            );
+            mainCharacter.attack = mainCharacter.attack + mainCharacter.attackIncrement;
+
+            if (mainCharacter.health <= 0) {
+                $('#status').append(
+                  '<h3>You have been defeated... GAME OVER!</h3>' +
+                  '<button id="restart">Restart</button>'
+                );
+                $('#attack').attr('disabled', true);
+            } else if (defender.health <= 0) {
+                $('#status').append(
+                  '<h3>You have defeated ' + defender.name + ', you can choose to fight another enemy.</h3>'
+                );
+                $('#attack').attr('disabled', true);
+            };
+        };
+    });
+
+
+    $(document).on('click', '#restart', function() {
+        $('.main-character').empty();
+        $('.defender').empty();
+        $('#status').empty();
+        mainCharacter = {};
+        defender = {};
+        $('#starting-characters').appendcreateCharacters();
+        $('#attack').attr('disabled', false);
+    });
+
 
     // push object.health of each defender to this array
     // var defenderHealthArray = [];
