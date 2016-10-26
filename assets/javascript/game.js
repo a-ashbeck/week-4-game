26d2be628004539718907adc57d2ea3208f8e381
Alan <a-ashbeck@northwestern.edu>
2016-10-25 22:29:36 -0500

    Refactor logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,47 +1,54 @@
 $(document).ready(function() {
     var lukeSkywalker = {
-        name: 'Luke Skywalker',
-        health: 100,
-        baseHealth: 100,
-        attack: 26,
-        attackIncrement: 26,
-        counterAttack: 6,
-        image: 'assets/images/luke.jpg'
-    };
+            name: 'Luke Skywalker',
+            health: 100,
+            baseHealth: 100,
+            attack: 26,
+            attackIncrement: 26,
+            counterAttack: 6,
+            image: 'assets/images/luke.jpg'
+        },
 
-    var darthVader = {
-        name: 'Darth Vader',
-        health: 150,
-        baseHealth: 150,
-        attack: 8,
-        attackIncrement: 8,
-        counterAttack: 25,
-        image: 'assets/images/vader.jpg'
-    };
+        darthVader = {
+            name: 'Darth Vader',
+            health: 150,
+            baseHealth: 150,
+            attack: 8,
+            attackIncrement: 8,
+            counterAttack: 25,
+            image: 'assets/images/vader.jpg'
+        },
 
-    var obiWanKenobi = {
-        name: 'Obi-Wan Kenobi',
-        health: 120,
-        baseHealth: 120,
-        attack: 12,
-        attackIncrement: 12,
-        counterAttack: 10,
-        image: 'assets/images/kenobi.jpg'
-    };
+        obiWanKenobi = {
+            name: 'Obi-Wan Kenobi',
+            health: 120,
+            baseHealth: 120,
+            attack: 12,
+            attackIncrement: 12,
+            counterAttack: 10,
+            image: 'assets/images/kenobi.jpg'
+        },
 
-    var kyloRen = {
-        name: 'Kylo Ren',
-        health: 130,
-        baseHealth: 130,
-        attack: 10,
-        attackIncrement: 10,
-        counterAttack: 20,
-        image: 'assets/images/kylo.jpg'
-    };
+        kyloRen = {
+            name: 'Kylo Ren',
+            health: 130,
+            baseHealth: 130,
+            attack: 10,
+            attackIncrement: 10,
+            counterAttack: 20,
+            image: 'assets/images/kylo.jpg'
+        },
 
-    var mainCharacter = {};
-    var defender = {};
-    var winCounter = 0;
+        characterObject = {
+            'luke-skywalker': lukeSkywalker,
+            'darth-vader': darthVader,
+            'obi-wan-kenobi': obiWanKenobi,
+            'kylo-ren': kyloRen
+        },
+
+        mainCharacter = {},
+        defender = {},
+        winCounter = 0;
 
     function postWins() {
         $('#status-wins').empty();
@@ -49,9 +56,9 @@ $(document).ready(function() {
     }
 
     function renderDom() {
-      // $('body').append(
-      //   '<div class="container"><div class="row"><div class="col-md-12 col-sm-12 col-xs-12" id="starting-characters"></div></div><div class="row"><div class="col-md-12 col-sm-12 col-xs-12"><h2>Your Character:</h2></div><div class="col-md-3 col-sm-6 col-xs-12 main-character"></div></div><div class="row"><div class="col-md-12 col-sm-12 col-xs-12"><h2>Enemy Combatants:</h2></div><div class="col-md-12 col-sm-12 col-xs-12" id="enemies"></div></div><div class="row"><div class="col-md-12 col-sm-12 col-xs-12"><h2>Click the attack button to fight your opponent!</h2></div><div class="col-md-12 col-sm-12 col-xs-12" id="battle-zone"><button id="attack">Attack!</button></div></div><div class="row"><div class="col-md-12 col-sm-12 col-xs-12"><h2>Your Opponent:</h2></div><div class="col-md-3 col-sm-6 col-xs-12 defender"></div></div><div class="row"><div class="col-md-12 col-sm-12 col-xs-12"><h2>Status:</h2></div><div class="col-md-12 col-sm-12 col-xs-12" id="status"></div></div><div class="row restart-row"><div class="col-md-12 col-sm-12 col-xs-12"><h2>Restart the Game</h2></div><button id="restart">Restart</button></div></div>'
-      // );
+      $('body').append(
+        '<div class="jumbotron"><h1 class="text-center">STAR WARS</h1><p class="text-center lead">...the RPG</p></div><div class="container"><div class="row"><div class="col-md-12 col-sm-12 col-xs-12" id="starting-characters"></div></div><div class="row"><div class="col-md-4 col-sm-6 col-xs-12 main-character"></div><div id="stats" class="col-md-4 col-sm-12 col-xs-12"><div class="panel panel-default"><div class="panel-heading"><h2>Battle Zone</h2></div><div class="panel-body"><div class="panel panel-info"><div class="panel-heading"><h2 class="panel-title">Status:</h2></div><div class="panel-body"><div class="col-md-12 col-sm-12 col-xs-12"><p id="status-wins"></p><p>R2\'s Battle Update:</p><div id="status" class="well"></div></div></div></div><div class="col-md-12 col-sm-12 col-xs-12" id="battle-zone"><div class="restart-row"><button id="restart" class="btn btn-warning btn-lg btn-block">Restart the Game</button></div><h6>Click the attack button to fight your opponent!</h6><button id="attack" class="btn btn-danger btn-lg btn-block">Attack!</button></div></div></div></div><div class="col-md-4 col-sm-6 col-xs-12 defender pull-right"></div></div><div class="row"><div class="col-md-12 col-sm-12 col-xs-12"><h3>Enemy Combatants:</h3></div><div class="col-md-12 col-sm-12 col-xs-12" id="enemies"></div></div></div>'
+      );
       createCharacters();
       generateCharacterPanels();
       $('.restart-row').hide();
@@ -71,13 +78,6 @@ $(document).ready(function() {
         );
     }
 
-    var characterObject = {
-        'luke-skywalker': lukeSkywalker,
-        'darth-vader': darthVader,
-        'obi-wan-kenobi': obiWanKenobi,
-        'kylo-ren': kyloRen
-    };
-
     function generateCharacterPanels() {
         jQuery.each( characterObject, function( i, val ) {
             $('.' + i ).append(
@@ -88,6 +88,67 @@ $(document).ready(function() {
                 '<div class="panel-footer">' + val.health + '</div></div>'
             );
         });
+    }
+
+    function generateMainCharacter() {
+        $('.main-character').append(
+            '<h3>Your Character:</h3>' +
+            '<div class="panel panel-primary"><div class="panel-heading">' +
+            mainCharacter.name + '</div>' +
+            '<div class="panel-body"><img class="img-responsive" src="' +
+            mainCharacter.image + '"></div>' +
+            '<div class="panel-footer">' + mainCharacter.health +
+            '</div></div>'
+        );
+    }
+
+    function generateDefender() {
+        $('.defender').append(
+            '<h3>Your Opponent:</h3>' +
+            '<div class="panel panel-danger"><div class="panel-heading">' +
+            defender.name + '</div>' +
+            '<div class="panel-body"><img class="img-responsive" src="' +
+            defender.image + '"></div>' +
+            '<div class="panel-footer">' + defender.health +
+            '</div></div>'
+        );
+    }
+
+    function gameLost() {
+        $('#status').append(
+          '<div class="alert alert-dismissible alert-danger">' +
+          'You have been defeated... <strong>GAME OVER!</strong></div>'
+        );
+        $('#attack').attr('disabled', true);
+        $('.restart-row').show();
+    }
+
+    function battleWon() {
+        winCounter++;
+        postWins();
+        $('#status').append(
+          '<div class="alert alert-success">You have defeated ' +
+          defender.name + ', you can choose to fight another enemy.</div>'
+        );
+        $('#attack').attr('disabled', true);
+        $('.defender').empty();
+        defender = {};
+        disableAttackBtn();
+    }
+
+    function disableAttackBtn() {
+        $('#attack').attr('disabled', false);
+    }
+
+    function wasGameWon() {
+        if (winCounter === 3) {
+            $('#status').append('<h3>You Won the Game!</h3>');
+            $('.restart-row').show();
+        };
+    }
+
+    function emptyStatusPanel() {
+        $('#status').empty();
     }
 
     renderDom();
@@ -117,16 +178,7 @@ $(document).ready(function() {
                 $('.obi-wan-kenobi').appendTo('#enemies');
             };
 
-            $('.main-character').append(
-                '<h3>Your Character:</h3>' +
-                '<div class="panel panel-primary"><div class="panel-heading">' +
-                mainCharacter.name + '</div>' +
-                '<div class="panel-body"><img class="img-responsive" src="' +
-                mainCharacter.image + '"></div>' +
-                '<div class="panel-footer">' + mainCharacter.health +
-                '</div></div>'
-            );
-
+            generateMainCharacter();
             $('#starting-characters').empty();
             $(this).addClass('chosen-character');
         } else if (
@@ -154,50 +206,25 @@ $(document).ready(function() {
             };
 
             $(this).hide();
-
-            $('.defender').append(
-                '<h3>Your Opponent:</h3>' +
-                '<div class="panel panel-danger"><div class="panel-heading">' +
-                defender.name + '</div>' +
-                '<div class="panel-body"><img class="img-responsive" src="' +
-                defender.image + '"></div>' +
-                '<div class="panel-footer">' + defender.health +
-                '</div></div>'
-            );
+            generateDefender();
         };
     });
 
     $('#attack').on('click', function() {
         if (Object.keys(defender).length === 0) {
-            $('#status').empty();
+            emptyStatusPanel();
             $('#status').append(
                 '<h4>No enemy here</h4>'
             );
         } else {
-            $('#status').empty();
+            emptyStatusPanel();
             $('.main-character').empty();
             $('.defender').empty();
             defender.health = defender.health - mainCharacter.attack;
             mainCharacter.health = mainCharacter.health -
                 defender.counterAttack;
-            $('.main-character').append(
-                '<h3>Your Character:</h3>' +
-                '<div class="panel panel-primary"><div class="panel-heading">' +
-                mainCharacter.name + '</div>' +
-                '<div class="panel-body"><img class="img-responsive" src="' +
-                mainCharacter.image + '"></div>' +
-                '<div class="panel-footer">' + mainCharacter.health +
-                '</div></div>'
-            );
-            $('.defender').append(
-                '<h3>Your Opponent:</h3>' +
-                '<div class="panel panel-danger"><div class="panel-heading">' +
-                defender.name + '</div>' +
-                '<div class="panel-body"><img class="img-responsive" src="' +
-                defender.image + '"></div>' +
-                '<div class="panel-footer">' + defender.health +
-                '</div></div>'
-            );
+            generateMainCharacter();
+            generateDefender();
             $('#status').append(
                 '<p>You attacked ' + defender.name + ' for ' +
                 mainCharacter.attack +
@@ -214,38 +241,6 @@ $(document).ready(function() {
             };
         };
     });
-
-    function gameLost() {
-        $('#status').append(
-          '<div class="alert alert-dismissible alert-danger">' +
-          'You have been defeated... <strong>GAME OVER!</strong></div>'
-        );
-        $('#attack').attr('disabled', true);
-        $('.restart-row').show();
-    }
-    function battleWon() {
-        winCounter++;
-        postWins();
-        $('#status').append(
-          '<div class="alert alert-success">You have defeated ' +
-          defender.name + ', you can choose to fight another enemy.</div>'
-        );
-        $('#attack').attr('disabled', true);
-        $('.defender').empty();
-        defender = {};
-        disableAttackBtn();
-    }
-
-    function disableAttackBtn() {
-        $('#attack').attr('disabled', false);
-    }
-
-    function wasGameWon() {
-        if (winCounter === 3) {
-            $('#status').append('<h3>You Won the Game!</h3>');
-            $('.restart-row').show();
-        };
-    }
 
     // Function to restart the game at any given moment
     $('#restart').on('click', function() {
